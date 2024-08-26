d45e33da27e696b4d7b8dfb75a1ec514ceaa3f2e
Raphael Okafor <30964110+Antobrige@users.noreply.github.com>
2024-08-27 00:59:39 +0100

    okason first job

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 import Image from "next/image";
 import { Hero } from "./components/Hero";
+import { Navbar } from "./components/Navbar";
 
 export default function Home(){
   return(
-    <Hero/>
-  )
+    <div>
+    <Navbar/>
+      <Hero/>
+    </div>
+      
+
+
+  );
 }
